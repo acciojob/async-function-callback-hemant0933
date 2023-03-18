449493e954237e900435e94f35dd78b72f61b2d9
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-18 19:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+
+
+ async function fetchFunction(callback){
+	 const response1 = await fetch(url);
+    const data1 = await response1.json();
+// return document.getElementById('output').innerHTML = `<p>`${data1[0]}`</p>`
+	 callback(data1.title);
+}
+document.getElementById('btn').addEventListener('click', async () => {
+  const outputDiv = document.getElementById('output');
+  outputDiv.textContent = 'Loading...';
+  fetchFunction((title) => {
+    outputDiv.textContent = title;
+  });
+});
